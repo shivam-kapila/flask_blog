e6a077cd69b44fea05ce9be53eaa4f3c173a31ab
shivam-kapila <shivamkapila4@gmail.com>
2019-10-14 01:46:47 +0530

    JSON sent

--- flaskblog.py
+++ flaskblog.py
@@ -1,11 +1,16 @@
 from flask import Flask, escape, request
+import json
 
 app = Flask(__name__)
 
 @app.route('/')
 @app.route('/home')
 def hello():
-    return "<h1>Hii</h1>"
+    blog = {
+        'name': '1',
+        'name2': '12' 
+    }
+    return json.dumps(blog)
 
 if __name__ == '__main__':
     app.run(debug=True)
